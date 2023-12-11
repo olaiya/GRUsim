97ba42b78ac97801bddbba3b7a495cd38b939aac
Emmanuel Olaiya <emmanuel.olaiya@stfc.ac.uk>
2023-12-11 20:00:58 +0000

    Written GRU Layer as the basis for a class and fix bug in GRU calc as a consequence of using np assignment rather than copy. Also changed use of i twice as the index for both loops in a nested loop

--- GRULayer.py
+++ GRULayer.py
@@ -3,115 +3,131 @@ import pandas as pd
 import math
 
 #Will tidy all this up and put in a class
+class GRULayer():
 
+  def __init__(self, inputFileName='xInput.txt',  matricesFileName='GRUMatrices.npz'):
+    self.setInputFile(inputFileName)
+    self.matricesFilename = matricesFileName
+    self.h_tminus1 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    self.verboseFlag = False 
+    self.loadMatrices()
+    self.setMatrices()
+    self.sigmoid_v = np.vectorize(self.sigmoid)
+    self.tanh_v =  np.vectorize(self.mytanh)
 
-# define a vectorised sigmoid function
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
+  def loadMatrices(self, matricesFileName='GRUMatrices.npz'):
+    #Load matrices
+    self.matrices = np.load(matricesFileName, mmap_mode=None, allow_pickle=False, fix_imports=True)
 
-sigmoid_v = np.vectorize(sigmoid)
+  def setInputFile(self, inputFileName):
+    self.inputFileName = inputFileName
+    #open text file and read xValue inputs
+    self.data = np.genfromtxt(self.inputFileName, delimiter=' ')
 
-# define a vectorised tanh function
-def mytanh(x):
-  return math.tanh(x)
+  # define a vectorised sigmoid function
+  def sigmoid(self, x):
+    return 1 / (1 + math.exp(-x))
 
-tanh_v =  np.vectorize(mytanh)
+  # define a vectorised tanh function
+  def mytanh(self, x):
+    return math.tanh(x)
 
-#Load matrices
+  
 
-matrices = np.load('GRUMatrices.npz', mmap_mode=None, allow_pickle=False, fix_imports=True)
+  def setVerboseFlag(self, vbFlag):
+    self.verboseFlag = vbFlag
 
-#Get W, U snd bias matrices
-wMatrix = matrices['wMatrix']
-uMatrix = matrices['uMatrix']
-biases = matrices['biases']
+  def setMatrices(self):
+    #Get W, U snd bias matrices
+    wMatrix = self.matrices['wMatrix']
+    uMatrix = self.matrices['uMatrix']
+    biases = self.matrices['biases']
 
-#Separate matrices into their z,r and h components  
-wZ = wMatrix[:,0:6]
-wR = wMatrix[:,6:12]
-wH = wMatrix[:,12:18]
+    #Separate matrices into their z,r and h components  
+    self.wZ = wMatrix[:,0:6]
+    self.wR = wMatrix[:,6:12]
+    self.wH = wMatrix[:,12:18]
 
-uZ = uMatrix[:,0:6]
-uR = uMatrix[:,6:12]
-uH = uMatrix[:,12:18]
+    self.uZ = uMatrix[:,0:6]
+    self.uR = uMatrix[:,6:12]
+    self.uH = uMatrix[:,12:18]
 
-rbZ = biases[0:6]
-rbR = biases[6:12]
-rbH = biases[12:18]
+    self.rbZ = biases[0:6]
+    self.rbR = biases[6:12]
+    self.rbH = biases[12:18]
 
-'''
-ibZ = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-ibR = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-ibH = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-'''
-ibZ = biases[0,0:6]
-ibR = biases[0,6:12]
-ibH = biases[0,12:18]
+    self.ibZ = biases[0,0:6]
+    self.ibR = biases[0,6:12]
+    self.ibH = biases[0,12:18]
 
-rbZ = biases[1,0:6]
-rbR = biases[1,6:12]
-rbH = biases[1,12:18]
+    self.rbZ = biases[1,0:6]
+    self.rbR = biases[1,6:12]
+    self.rbH = biases[1,12:18]
 
-print('\nW matrices')
-print(wMatrix)
-print('\nwZ')
-print(wZ)
-print('\nwR')
-print(wR)
-print('\nwH')
-print(wH)
+    if (self.verboseFlag):
 
-print('\nU matrices')
-print(uMatrix)
-print('\nuZ')
-print(uZ)
-print('\nuR')
-print(uR)
-print('\nuH')
-print(uH)
+      print('\nW matrices')
+      print(wMatrix)
+      print('\nwZ')
+      print(wZ)
+      print('\nwR')
+      print(wR)
+      print('\nwH')
+      print(wH)
 
-print('\nbias matrices')
-print(biases)
-print('\nibZ')
-print(ibZ)
-print('\nibR')
-print(ibR)
-print('\nibH')
-print(ibH)
-print('\nrbZ')
-print(rbZ)
-print('\nrbR')
-print(rbR)
-print('\nrbH')
-print(rbH)
+      print('\nU matrices')
+      print(uMatrix)
+      print('\nuZ')
+      print(uZ)
+      print('\nuR')
+      print(uR)
+      print('\nuH')
+      print(uH)
 
-#open text file and read xValue inputs
-data = np.genfromtxt('xInput.txt', delimiter=' ')
+      print('\nbias matrices')
+      print(biases)
+      print('\nibZ')
+      print(ibZ)
+      print('\nibR')
+      print(ibR)
+      print('\nibH')
+      print(ibH)
+      print('\nrbZ')
+      print(rbZ)
+      print('\nrbR')
+      print(rbR)
+      print('\nrbH')
+      print(rbH)
 
-print(data.shape)
-h_tminus1 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-for i in range(data.shape[0]):
-  x_t=data[i]
-  #Calculate components of GRU
-  #x_t = np.array([-0.10292942821979523, -0.7129440307617188, 0.3375035226345062, 0.10583724081516266, -0.1298450380563736, 0.28884604573249817])
+  def run(self):
+    for i in range(self.data.shape[0]):
+      x_t=self.data[i]
+      #Calculate components of GRU
+      z_t =  np.matmul(np.transpose(self.wZ),x_t) + np.matmul(np.transpose(self.uZ), self.h_tminus1) + self.ibZ + self.rbZ
+      z_t = self.sigmoid_v(z_t)
 
-  z_t =  np.matmul(np.transpose(wZ),x_t) + np.matmul(np.transpose(uZ), h_tminus1) + ibZ + rbZ
-  z_t = sigmoid_v(z_t)
+      r_t = np.matmul(np.transpose(self.wR),x_t) + np.matmul(np.transpose(self.uR), self.h_tminus1) + self.ibR + self.rbR
+      r_t = self.sigmoid_v(r_t)
 
-  r_t = np.matmul(np.transpose(wR),x_t) + np.matmul(np.transpose(uR), h_tminus1) + ibR + rbR
-  r_t = sigmoid_v(r_t)
+      uH_update = np.copy(self.uH)
+      for j in range(self.uH.shape[0]):
+        uH_update[:,j] = self.uH[:,j] * r_t[j]
+        
 
-  uH_update = uH
-  for i in range(uH.shape[0]):
-    uH_update[:,i] = uH[:,i] * r_t[i]
+      h_that = np.matmul(np.transpose(self.wH), x_t) + np.matmul(np.transpose(uH_update), self.h_tminus1) + self.ibH + (self.rbH * r_t)
+      h_that = self.tanh_v(h_that)
 
-  h_that = np.matmul(np.transpose(wH), x_t) + np.matmul(np.transpose(uH_update), h_tminus1) + ibH + (rbH * r_t)
-  h_that = tanh_v(h_that)
+      #h = np.multiply(z_t, h_that) + np.multiply((1 - z_t),h_tminus1) 
+      h = np.multiply((1-z_t), h_that) + np.multiply(z_t, self.h_tminus1) 
+      self.h_tminus1 = h
+      print(h)
 
-  #h = np.multiply(z_t, h_that) + np.multiply((1 - z_t),h_tminus1) 
-  h = np.multiply((1-z_t), h_that) + np.multiply(z_t, h_tminus1) 
-  h_tminus1 = h
-  print(h)
+if __name__ == '__main__':
+  print('In the Main')
+
+  myGRULayer = GRULayer()
+  myGRULayer.run()
+
 
 #https://kaixih.github.io/keras-cudnn-rnn/
 #https://gist.github.com/bzamecnik/bd3786a074f8cb891bc2a397343070f1
